69d81a0b287bc15fc64397386f935ad26c2cd6ff
DESKTOP-M7LA5BV\HSY <huangshayang@supeq.com>
2017-07-26 19:13:18 +0800

    add SuiteTest

---
+++ src/test/Java/InitSuiteTest.java
@@ -0,0 +1,10 @@
+package Java;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({LoginCaseTest.class,CheckUserExistCaseTest.class})
+public class InitSuiteTest {
+
+}
